34123a68171200f0de3a4716ee9cc5bbb91a8962
AdityaMeena001 <adityameena1one@gmail.com>
2023-11-30 17:52:10 +0530

    added files

--- src/components/HomeProject.jsx
+++ src/components/HomeProject.jsx
@@ -8,9 +8,8 @@ const projectData = [
     name: "WonderNook",
     title: "Full Stack Web App",
     github_url: "https://github.com/AdityaMeena001/WonderNook",
-    deployed_url: "https://yourweatherapp.com",
-    screenshot_url:
-      "wonder-nook.png",
+    deployed_url: "https://wonder-nook.aditya-meena.live/",
+    screenshot_url: "wonder-nook.png",
   },
   {
     id: 2,
@@ -18,8 +17,7 @@ const projectData = [
     title: "Ai Comic Creator",
     github_url: "https://github.com/AdityaMeena001/Engineer-Product-Assignment",
     deployed_url: "https://comic-gen.aditya-meena.live/",
-    screenshot_url:
-      "comic-gen.png",
+    screenshot_url: "comic-gen.png",
   },
   {
     id: 3,
@@ -27,29 +25,38 @@ const projectData = [
     title: "Quicksell FrontEnd Assignment",
     github_url: "https://github.com/AdityaMeena001/Quicksell-Frontend-Assignment",
     deployed_url: "https://quicksell.aditya-meena.live/",
-    screenshot_url:
-      "quick-sell.png",
+    screenshot_url: "quick-sell.png",
   },
   {
     id: 4,
     name: "Portfolio Website",
-    title: "Full stack Web app",
+    title: "Branding / Personal Portfolio",
     github_url: "https://github.com/AdityaMeena001",
-    deployed_url: "https://aditya-meena.live",
-    screenshot_url:
-      "wonder-nook.png",
+    deployed_url: "https://www.aditya-meena.live",
+    screenshot_url: "portfolio.png",
+  },
+  {
+    id: 5,
+    name: "Creative Coding",
+    title: "Collection of My Code Sketches",
+    github_url: "https://github.com/AdityaMeena001/Creative-Coding",
+    deployed_url: "",
+    screenshot_url: "creative-coding.png",
   },
 ];
 
 const HomeProject = () => {
-  
   return (
     <div className="px-full py-8 border">
       <div className="flex justify-center pb-8  ">
         <h2>Projects</h2>
       </div>
 
-      <motion.div initial={{opacity:0}} whileInView={{opacity:1}} className=" justify-between grid gap-6 grid-cols-1  lg:grid-cols-2 w-full ">
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        className=" justify-between grid gap-6 grid-cols-1  lg:grid-cols-2 w-full "
+      >
         {projectData.map((project) => (
           <ProjectCard project={project} />
         ))}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,16 +9,16 @@ const Home = () => {
     <div className="text-black bg-[#FAFAFA] w-full max-w-[1880px]">
       <Navbar />
 
-      <div className="w-full items-center border p-8 h-96 px-full flex-col lg:flex-row flex justify-between">
-        <div>
+      <div className="w-full items-center border p-8 h-96 px-full flex justify-center">
+        <div className="flex flex-col items-center">
           <h4 className=" sm:text-lg" >Hi, I'm Aditya Meena</h4>
-          <h1>
+          <h1 className=" text-center">
             Creative Developer <br />
             based in India.
           </h1>
           {/* <p>Write something here.</p> */}
         </div>
-        <div className=" w-full mt-4 lg:m-0 md:w-3/4 lg:w-1/2 h-full rounded-full bg-red-300"></div>
+        {/* <div className=" w-full mt-4 lg:m-0 md:w-3/4 lg:w-1/2 h-full rounded-full bg-red-300"></div> */}
       </div>
 
       <div id="Projects">
